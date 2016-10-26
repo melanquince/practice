8bf38598f7213ef8fd9591d354b04f37f6245f8a
Dmitry Bryukhovetsky <melon.even.quince@gmail.com>
2016-10-26 19:57:10 +0300

    added pixel painter

--- jenniferdewalt/challenges/pixel_painter/js/act.js
+++ jenniferdewalt/challenges/pixel_painter/js/act.js
@@ -10,20 +10,34 @@ var paint_style = getComputedStyle(painting); // take all css values
 canvas.width = parseInt(paint_style.getPropertyValue('width')); 
 canvas.height = parseInt(paint_style.getPropertyValue('height'));
 
-var mouse = {x: 0, y: 0, size: 0, color: 0 };
+var mouse = {x: 0, y: 0, size: getSize(), color: getColor()};
 
  
 canvas.addEventListener('mousemove', function(e) {
   mouse.x = e.pageX - this.offsetLeft;
   mouse.y = e.pageY - this.offsetTop;
-  console.log(mouse.x + " " + mouse.y);
 });
 
-mouse.color = getColor();
-mouse.size  = getSize();
+ctx.lineWidth = mouse.size;
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.strokeStyle = mouse.color;
+
+
+function toRGBA(clr) {
+	var clrArr = clr.split("");
+		function toDecimal(num1,num2) {
+			return parseInt((clrArr[num1] + clrArr[num2]),16);
+		}
+		var red = toDecimal(0,1);
+		var green = toDecimal(2,3);
+		var blue = toDecimal(4,5);
+		return red+","+green +","+blue;
+}
 
 function getColor() {
-	return "#"+intColor.value;
+	var opc="0.4";
+	return "rgba("+toRGBA(intColor.value)+","+opc+")";
 }
 
 function getSize() {
@@ -36,50 +50,55 @@ canvas.addEventListener('click', function() {
 
 function mouseDraw() {
 	ctx.beginPath();
-	ctx.arc(mouse.x,mouse.y,mouse.size,0,Math.PI*2, true);
+	ctx.arc(mouse.x,mouse.y,mouse.size/2,0,Math.PI*2, true);
 	ctx.fillStyle = mouse.color;
 	ctx.fill();
 	ctx.closePath();
 
 }
 
-// function mouseMove() {
-// 	ctx.clearRect(0,0,canvas.width,canvas.height);
-// 	ctx.beginPath();
-// 	ctx.arc(mouse.x,mouse.y,mouse.size,0,2*Math.PI,true);
-// 	ctx.fillStyle = mouse.color;
-// 	ctx.fill();
-	
-// }
-// mouseMove();
+// events for canvas
 
 canvas.addEventListener('mousedown', function(e) {
-	canvas.addEventListener('mousemove', mouseDraw );
-	//requestAnimationFrame();
- 	
+	ctx.beginPath();
+    ctx.moveTo(mouse.x, mouse.y);
+    canvas.addEventListener('mousemove', onPaint);
  });
 
 canvas.addEventListener('mouseup', function() {
+	canvas.removeEventListener('mousemove', onPaint);
 	canvas.removeEventListener('mousemove', mouseDraw);
 		
 });
 
 canvas.addEventListener('mouseleave', function() {
+	canvas.removeEventListener('mousemove', onPaint);
 	canvas.removeEventListener('mousemove', mouseDraw);
 });
 
-// events to buttons and inputs
+var onPaint = function() {
+    ctx.lineTo(mouse.x, mouse.y);
+    ctx.stroke();
+};
+
+
+// events for buttons and inputs
 
 document.getElementById('eraser_all').addEventListener("click", function() {
 	ctx.clearRect(0,0,canvas.width,canvas.height);
 });
 document.getElementById('eraser').addEventListener("click", function() {
-	mouse.color = "#ffffff";
+	mouse.color = "#ffffff"
 	mouse.size = 35;
+	ctx.lineWidth = 35;
+	ctx.strokeStyle = mouse.color;
+	
 });
 
 intColor.addEventListener("focusout", function() {
 	mouse.color = getColor();
+	ctx.strokeStyle = mouse.color;
+
 });
 
 intSize.addEventListener("focusout", function() {
@@ -88,6 +107,7 @@ intSize.addEventListener("focusout", function() {
 		alert("Size must be a number!");
 	} else {
 		mouse.size = a;
+		ctx.lineWidth = mouse.size;
 	}
 
 });
